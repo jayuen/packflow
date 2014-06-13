0861281513532a952b17d47d47e9a348b35c93ca
Clemens Park <clemens.park@gmail.com>
2014-06-13 13:43:32 -0400

    shows the info from the initial data

--- app/assets/javascripts/configs_module.js
+++ app/assets/javascripts/configs_module.js
@@ -1,8 +1,18 @@
 var m = angular.module('configsApp', []);
 
-var ConfigsCtrl = function() {
+var ConfigsCtrl = function(initialData) {
+  this.workflows = initialData.workflows;
+  this.questions = initialData.workflows;
+  this.workflowDrivers = initialData.workflows;
+
+  this.getWorkflowFromDriver = function(driver) {
+  }
+
+  this.getQuestionFromDriver = function(driver) {
+  }
 };
 
 m.controller("ConfigsCtrl", [
+  "initialData",
   ConfigsCtrl
 ]);
