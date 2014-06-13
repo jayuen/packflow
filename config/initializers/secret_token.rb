e446ab11aeb1db6608ff1defe8c8c858c6729d67
Jason Yuen <jasony@nulogy.com>
2014-06-12 22:51:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Packflow::Application.config.secret_token = '9b4b0debeacd024bfac258dff17bae85c734355c97b37a660587945dc8d51742b57e34702c72ac2955221807367576ce564f57cfee2c2e158ef0570baff3bcac'
