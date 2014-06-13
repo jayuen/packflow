ff0449e2fa84ef8d576a9484ddaf13bca3940851
Clemens Park <clemens.park@gmail.com>
2014-06-13 16:14:19 -0400

    adds setting_type to setting drivers (site vs account setting)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140613031957) do
+ActiveRecord::Schema.define(version: 20140613201107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140613031957) do
     t.text     "answer"
     t.text     "setting"
     t.text     "setting_value"
+    t.text     "setting_type"
   end
 
   create_table "workflow_drivers", force: true do |t|
