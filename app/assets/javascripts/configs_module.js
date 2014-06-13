406d60ed765346383f678be12059c6483a8ef89d
Clemens Park <clemens.park@gmail.com>
2014-06-13 14:33:11 -0400

    supports adding new workflows

--- app/assets/javascripts/configs_module.js
+++ app/assets/javascripts/configs_module.js
@@ -1,20 +1,35 @@
 var m = angular.module('configsApp', []);
 
-var ConfigsCtrl = function(initialData) {
+var ConfigsCtrl = function($http, initialData) {
   this.workflows = initialData.workflows;
   this.questions = initialData.questions;
   this.workflowDrivers = initialData.workflow_drivers;
+  this.newWorkflow = {};
 
   this.getWorkflowFromDriver = function(driver) {
     return _.findWhere(this.workflows, { id: driver.workflow_id });
-  }
+  };
 
   this.getQuestionFromDriver = function(driver) {
     return _.findWhere(this.questions, { id: driver.question_id });
-  }
+  };
+
+  this.addNewWorkflow = function() {
+    $http.post("/configs/add_new_workflow", this.newWorkflow).then(_.bind(onSuccess, this));
+
+    function onSuccess(xhr) {
+      this.workflows.push(xhr.data);
+      this.resetNewWorkflow();
+    }
+  };
+
+  this.resetNewWorkflow = function() {
+    this.newWorkflow = {};
+  };
 };
 
 m.controller("ConfigsCtrl", [
+  "$http",
   "initialData",
   ConfigsCtrl
 ]);
--- app/controllers/configs_controller.rb
+++ app/controllers/configs_controller.rb
@@ -4,4 +4,9 @@ class ConfigsController < ApplicationController
     @questions = Question.all.to_json
     @workflow_drivers = WorkflowDriver.all.to_json
   end
+
+  def add_new_workflow
+    new_workflow = Workflow.create! params.permit(:code, :description)
+    render json: new_workflow 
+  end
 end
