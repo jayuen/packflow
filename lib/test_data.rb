a5e113a8febe1e9889c6b365a695d46d6ac4e8cb
Jason Yuen <jayuen@gmail.com>
2014-06-13 14:55:32 -0400

    updated test data

--- lib/test_data.rb
+++ lib/test_data.rb
@@ -7,16 +7,25 @@ module TestData
     Question.destroy_all
     SettingDriver.destroy_all
 
-    workflow_a = Workflow.create! code: 'A', description: 'Planned receiving'
-    workflow_b = Workflow.create! code: 'B', description: 'Unplanned receiving'
+    workflow_1 = Workflow.create! code: 'W1', description: 'Mobile + Planned Receipt'
+    workflow_2 = Workflow.create! code: 'W2', description: 'Mobile + No Planned Receipt'
+    workflow_3 = Workflow.create! code: 'W3', description: 'No Mobile + No Planned Receipt'
+    workflow_4 = Workflow.create! code: 'W4', description: 'No Mobile + Planned Receipt'
 
-    question_1 = Question.create! code: 'Q1', title: 'What colour do you like?'
-    question_2 = Question.create! code: 'Q2', title: 'What is your favourite food?'
+    question_1 = Question.create! code: '1', title: 'Are you able to use mobile devices on the receiving floor?'
+    question_2 = Question.create! code: '2', title: 'Do you get ASNs for what is expected on a truck?'
+    question_3 = Question.create! code: '3', title: 'Do you record time arrived?'
 
-    WorkflowDriver.create! workflow: workflow_a, answer: 'Y', question: question_1 
-    WorkflowDriver.create! workflow: workflow_a, answer: 'N', question: question_2 
+    WorkflowDriver.create! workflow: workflow_1, answer: 'Y', question: question_1 
+    WorkflowDriver.create! workflow: workflow_1, answer: 'Y', question: question_2 
 
-    WorkflowDriver.create! workflow: workflow_b, answer: 'N', question: question_1 
-    WorkflowDriver.create! workflow: workflow_b, answer: 'Y', question: question_2 
+    WorkflowDriver.create! workflow: workflow_2, answer: 'Y', question: question_1 
+    WorkflowDriver.create! workflow: workflow_2, answer: 'N', question: question_2 
+
+    WorkflowDriver.create! workflow: workflow_3, answer: 'N', question: question_1 
+    WorkflowDriver.create! workflow: workflow_3, answer: 'N', question: question_2 
+
+    WorkflowDriver.create! workflow: workflow_4, answer: 'N', question: question_1 
+    WorkflowDriver.create! workflow: workflow_4, answer: 'Y', question: question_2 
   end
 end
