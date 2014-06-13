bf9091a6caf1f3501c650cef3c248391fb0e2e79
Jason Yuen <jayuen@gmail.com>
2014-06-13 01:48:34 -0400

    adds code to workflow

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20140613031957) do
   create_table "workflows", :force => true do |t|
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.text     "code"
     t.text     "description"
   end
 
